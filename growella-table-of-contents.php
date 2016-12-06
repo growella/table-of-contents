c673e07eeb6e36e568547fbb3cabc55a63250deb
Steve Grunwell <stevegrunwell@gmail.com>
2016-12-06 14:11:10 -0500

    Begin laying out the plugin, starting with the logic that parses content and injects IDs

---
+++ growella-table-of-contents.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Growella Table of Contents
+ * Description: Automatically generate and render a Table of Contents for a post.
+ * Version:     0.1.0
+ * Author:      Growella
+ * Author URI:  https://growella.com
+ * License:     MIT
+ *
+ * @package Growella\TableOfContents
+ * @author  Growella
+ */
+
+namespace Growella\TableOfContents;
+
+require_once __DIR__ . '/includes/core.php';
---
+++ tests/bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Bootstrap the test suite.
+ *
+ * @package Growella\TableOfContents
+ * @author  Growella
+ */
+
+if ( ! defined( 'PROJECT' ) ) {
+	define( 'PROJECT', __DIR__ . '/../includes/' );
+}
+
+if ( ! file_exists( __DIR__ . '/../vendor/autoload.php' ) ) {
+	throw new PHPUnit_Framework_Exception(
+		'ERROR: You must use Composer to install the test suite\'s dependencies!' . PHP_EOL
+	);
+}
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/test-tools/TestCase.php';
+
+WP_Mock::setUsePatchwork( true );
+WP_Mock::bootstrap();
+WP_Mock::tearDown();
