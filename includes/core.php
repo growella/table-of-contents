8a73f07b85274bd80011cb4a2bf3b12d4f05c9e1
Steve Grunwell <stevegrunwell@gmail.com>
2016-12-08 13:50:06 -0500

    Very preliminary rendering of a table of contents

--- includes/core.php
+++ includes/core.php
@@ -8,6 +8,8 @@
 
 namespace Growella\TableOfContents\Core;
 
+use Growella\TableOfContents\Headings as Headings;
+
 /**
  * Render the Table of Contents.
  *
@@ -15,6 +17,66 @@ namespace Growella\TableOfContents\Core;
  * @return string The rendered table of contents.
  */
 function render_shortcode( $atts ) {
-	return '';
+	$atts  = shortcode_atts( array(
+		'tags' => 'h1,h2,h3',
+	), $atts, 'toc' );
+
+	// Parse the post content to get IDs.
+	$content = new \DOMDocument();
+	$content->loadHTML( Headings\inject_heading_ids( get_the_content(), $atts ), LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD );
+
+	// We'll parse the document using DOMXpath to get any of the whitelisted tags with ID attributes.
+	$xpath = new \DOMXpath( $content );
+	$query = array();
+
+	foreach ( explode( ',', (string) $atts['tags'] ) as $tag ) {
+		$query[] = sprintf( '//%s[@id]', $tag );
+	}
+
+	$headings = $xpath->query( implode( '|', $query ) );
+
+	// Return early if we don't have any headings.
+	if ( 0 === $headings->length ) {
+		return;
+	}
+
+	// Assemble our output.
+	$links = build_link_list( $headings );
+
+	if ( empty( $links ) ) {
+		return;
+	}
+
+	$output  = '<nav class="growella-table-of-contents">';
+	$output .= '<ul>';
+
+	foreach ( $links as $link ) {
+		$output .= '<li>' . $link . '</li>';
+	}
+
+	$output .= '</ul>';
+	$output .= '</nav>';
+
+	return $output;
 }
 add_shortcode( 'toc', __NAMESPACE__ . '\render_shortcode' );
+
+/**
+ * Given a DOMNodeList, assemble an array of links to the IDs of each matched element.
+ *
+ * @param DOMNodeList $list A DOMNodeList to iterate through.
+ * @return array A flat array of links.
+ */
+function build_link_list( $list ) {
+	$links = array();
+
+	foreach ( $list as $item ) {
+		$links[] = sprintf(
+			'<a href="#%s">%s</a>',
+			esc_attr( $item->getAttribute( 'id' ) ),
+			$item->nodeValue
+		);
+	}
+
+	return $links;
+}
--- tests/PHPUnit/CoreTest.php
+++ tests/PHPUnit/CoreTest.php
@@ -15,11 +15,112 @@ class CoreTest extends \Growella\TableOfContents\TestCase {
 
 	protected $testFiles = array(
 		'core.php',
+		'headings.php',
 	);
 
 	public function setUp() {
 		M::wpPassthruFunction( 'add_shortcode' );
 
 		parent::setUp();
+	}
+
+	public function testRenderShortcode() {
+		$content  = <<<EOT
+<h2 id="first-heading">First heading</h2>
+<p>Paragraph</p>
+<h3 id="sub-heading">Sub-heading</h3>
+<p>Another paragraph</p>
+<h2 id="second-heading">Second heading</h2>
+<p>Last paragraph</p>
+EOT;
+		$expected  = '<nav class="growella-table-of-contents"><ul>';
+		$expected .= '<li><a href="#first-heading">First heading</a></li>';
+		$expected .= '<li><a href="#sub-heading">Sub-heading</a></li>';
+		$expected .= '<li><a href="#second-heading">Second heading</a></li>';
+		$expected .= '</ul></nav>';
+
+		M::wpFunction( 'shortcode_atts', array(
+			'return' => array(
+				'tags' => 'h1,h2,h3',
+			),
+		) );
+
+		M::wpFunction( 'get_the_content', array(
+			'return' => $content,
+		) );
+
+		M::wpFunction( __NAMESPACE__ . '\build_link_list', array(
+			'return' => array(
+				'<a href="#first-heading">First heading</a>',
+				'<a href="#sub-heading">Sub-heading</a>',
+				'<a href="#second-heading">Second heading</a>',
+			),
+		) );
+
+		M::wpPassthruFunction( 'Growella\TableOfContents\Headings\inject_heading_ids' );
+
+		$this->assertEquals( $expected, render_shortcode( array() ) );
+	}
+
+	public function testRenderShortcodeReturnsEmptyNullIfNoLinksFound() {
+		M::wpFunction( 'shortcode_atts', array(
+			'return' => array(
+				'tags' => 'h1,h2,h3',
+			),
+		) );
+
+		M::wpFunction( 'get_the_content', array(
+			'return' => 'no links here',
+		) );
+
+		M::wpFunction( __NAMESPACE__ . '\build_link_list', array(
+			'return' => array(),
+		) );
+
+		M::wpPassthruFunction( 'Growella\TableOfContents\Headings\inject_heading_ids' );
+
+		$this->assertNull( render_shortcode( array() ) );
+	}
+
+	public function testBuildLinkList() {
+		$dom   = new \DOMDocument;
+		$dom->loadHTML( '<h1 id="my-heading">My heading</h1>', LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD );
+		$xpath = new \DOMXpath( $dom );
+
+		$expected = array(
+			'<a href="#my-heading">My heading</a>'
+		);
+
+		M::wpPassthruFunction( 'esc_html' );
+		M::wpPassthruFunction( 'esc_attr' );
+
+		$this->assertEquals( $expected, build_link_list( $xpath->query( '//h1[@id]' ) ) );
+	}
+
+	public function testBuildLinkListWithMultipleHeadings() {
+				$content  = <<<EOT
+<div>
+	<h2 id="first-heading">First heading</h2>
+	<p>Paragraph</p>
+	<h3 id="sub-heading">Sub-heading</h3>
+	<p>Another paragraph</p>
+	<h2 id="second-heading">Second heading</h2>
+	<p>Last paragraph</p>
+</div>
+EOT;
+		$dom   = new \DOMDocument;
+		$dom->loadHTML( $content, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD );
+		$xpath = new \DOMXpath( $dom );
+
+		$expected = array(
+			'<a href="#first-heading">First heading</a>',
+			'<a href="#sub-heading">Sub-heading</a>',
+			'<a href="#second-heading">Second heading</a>',
+		);
+
+		M::wpPassthruFunction( 'esc_html' );
+		M::wpPassthruFunction( 'esc_attr' );
+
+		$this->assertEquals( $expected, build_link_list( $xpath->query( '//h2[@id]|h3[@id]' ) ) );
 	}
 }
